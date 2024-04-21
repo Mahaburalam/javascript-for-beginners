416944a295e52674ec8e060a4fa9bf4ebdc4ef58
Mahaburalam <mahaburcse@gmail.com>
2024-04-21 18:19:14 +0600

    number conversion

---
+++ Basic/number conversion/number-conversion.js
@@ -0,0 +1,18 @@
+// decimal to binary conversion
+const decimalNumber = 9672;
+console.log(`Convert decimal to binary number:`, decimalNumber.toString(2));
+
+// decimal to octal number conversion
+console.log(`Decimal to binary number conversion:`, decimalNumber.toString(8));
+
+console.log('----------------------------------------------')
+console.log('----------------------------------------------')
+// binary to decimal number conversion
+const binaryNumber = 0111;
+console.log(`Convert binary to decimal NUmber:`, binaryNumber.toString(10));
+
+// binary to octal number conversion
+console.log(`Binary number to octal number conversion:`, binaryNumber.toString(8));
+
+// binary to hexa-decinal number conversion
+console.log(`Binary to hexa-decimal number conversion:`, binaryNumber.toString(16))

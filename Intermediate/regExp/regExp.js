40bedf76bdb900932ba275f601fca5ff035af408
Mahaburalam <mahaburcse@gmail.com>
2024-05-09 16:31:32 +0600

    js regular expression pertially understand

---
+++ Intermediate/regExp/regExp.js
@@ -0,0 +1,8 @@
+const fullName = "Md Mahabur Alam";
+// string search
+const stringFind = fullName.search(/alam/i);
+console.log(stringFind);
+
+// string replace
+const stringReplace = fullName.replace(/alam/i, "Name");
+console.log(fullName);

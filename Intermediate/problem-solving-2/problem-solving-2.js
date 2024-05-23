c5609e2d19d693df2685e2fec15bca0ae21366e9
Mahaburalam <mahaburcse@gmail.com>
2024-05-23 17:12:56 +0600

    solve first problem

---
+++ Intermediate/problem-solving-2/problem-solving-2.js
@@ -0,0 +1,11 @@
+// from a sentence find name use in how many times and which position find first
+const sentence = "Hi, James went to the theater. James watched an engaging play. James then met the cast and director during the post-show discussion."
+
+// find match count
+const matchSearch = sentence.match(/james/gi);
+const matchSearchCount = matchSearch ? matchSearch.length : 0; // if search option match then return length else 0
+console.log(matchSearchCount);
+
+// first match position
+const position = sentence.search(/james/i); // search option return match position
+console.log(position);

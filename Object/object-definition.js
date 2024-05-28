7741212d290fe661ff0c0dad965b306d6c4d6ccb
Mahaburalam <mahaburcse@gmail.com>
2024-05-28 17:33:59 +0600

    object defination

---
+++ Object/object-definition.js
@@ -0,0 +1,16 @@
+// for non-primitive data type
+const person = {
+    name:"mahabur",
+    age:23
+}
+console.log("original object: ",person);
+
+person.name="Ahnaf";
+// person["name"]="Ahnaf";
+console.log("After updated: ", person);
+
+// for primitive data type
+let number = 12;
+console.log(number);
+number = 23;
+console.log(number);

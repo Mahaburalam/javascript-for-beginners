3319016d8b73dea7620f3806641e98d930649bf7
Mahaburalam <mahaburcse@gmail.com>
2024-06-06 15:12:23 +0600

    function

--- Function/function-parameter.js
+++ Function/function-parameter.js
@@ -3,3 +3,16 @@ function name(myName, herName) {
     return [myName, herName];
 }
 console.log(name("Mahabur Alam", "Nusrat Jahan", "Ahnaf Anas")); // this is arguments 
+
+// functions default parameter
+const sum = (x, y = 1) => {
+    // sum = (x + y);
+    return (x + y);
+}
+console.log(sum(7));
+
+const multiply = (x, y) => {
+    // sum = (x + y);
+    return (x * y);
+}
+console.log(multiply(5));

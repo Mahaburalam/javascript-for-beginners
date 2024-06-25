e57c59e96b5f1f1f820327e9500b0027380f2e87
Mahaburalam <mahaburcse@gmail.com>
2024-06-25 14:06:43 +0600

    function call

---
+++ Function/function-call.js
@@ -0,0 +1,18 @@
+const person = {
+    fullName: function(city, country) {
+        return( this.firstName + " " + this.lastName +" " + city + " " + country);
+    }
+}
+
+const person1 = {
+    firstName: "Mahabur",
+    lastName: "Alam"
+}
+
+const person2 = {
+    firstName: "Ahnaf",
+    lastName: "Anas"
+}
+
+console.log(person.fullName.call(person1));
+console.log(person.fullName.call(person2, "Khulna", "Bangladesh"));
